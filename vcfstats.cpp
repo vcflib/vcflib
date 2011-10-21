05db16949028d7b4f8c8979ab16028870160f154
Erik Garrison <erik.garrison@bc.edu>
2011-10-21 16:51:33 -0400

    vcfstats: allow the addition of statistics to the VCF file
    The --add-info flag allows the accumulated statistics to be appended to
    the VCF records.  This is useful in that the pairwise-alignment based
    statistics can be queried and tabulated in another context.

--- vcfstats.cpp
+++ vcfstats.cpp
@@ -33,12 +33,35 @@ bool isAmination(const string& ref, const string& alt) {
     }
 }
 
+class AlleleStats {
+public:
+    int transitions;
+    int transversions;
+    int deaminations;
+    int aminations;
+    int mismatches;
+    int insertions;
+    int deletions;
+    //AlleleStats(int ts, int tv, int da, int am, int mm)
+    AlleleStats(void)
+        : transitions(0)
+        , transversions(0)
+        , deaminations(0)
+        , aminations(0)
+        , mismatches(0)
+        , insertions(0)
+        , deletions(0)
+    { }
+};
+
 void printSummary(char** argv) {
     cerr << "usage: " << argv[0] << " [options] <vcf file>" << endl
          << endl
          << "    -r, --region          specify a region on which to target the stats, requires a BGZF" << endl
          << "                          compressed file which has been indexed with tabix.  any number of" << endl
          << "                          regions may be specified." << endl
+         << "    -a, --add-info        add the statistics intermediate information to the VCF file," << endl
+         << "                          writing out VCF records instead of summary statistics" << endl
          << endl
          << "Prints statistics about variants in the input VCF file." << endl;
 }
@@ -47,6 +70,7 @@ void printSummary(char** argv) {
 int main(int argc, char** argv) {
 
     vector<string> regions;
+    bool addTags = false;
 
     int c;
     while (true) {
@@ -56,13 +80,14 @@ int main(int argc, char** argv) {
             //{"verbose", no_argument,       &verbose_flag, 1},
             {"help", no_argument, 0, 'h'},
             {"region", required_argument, 0, 'r'},
+            {"add", no_argument, 0, 'a'},
             //{"length",  no_argument, &printLength, true},
             {0, 0, 0, 0}
         };
         /* getopt_long stores the option index here. */
         int option_index = 0;
 
-        c = getopt_long (argc, argv, "hr:",
+        c = getopt_long (argc, argv, "har:",
                          long_options, &option_index);
 
       /* Detect the end of the options. */
@@ -89,6 +114,10 @@ int main(int argc, char** argv) {
           case 'r':
             regions.push_back(optarg);
             break;
+
+          case 'a':
+            addTags = true;
+            break;
           
           default:
             abort ();
@@ -106,6 +135,17 @@ int main(int argc, char** argv) {
 
     if (!variantFile.is_open()) {
         return 1;
+    }
+
+    if (addTags) {
+        variantFile.addHeaderLine("##INFO=<ID=transitions,Number=A,Type=Integer,Description=\"Total number of transitions in the alternate allele\">");
+        variantFile.addHeaderLine("##INFO=<ID=transversions,Number=A,Type=Integer,Description=\"Total number of transversions in the alternate allele\">");
+        variantFile.addHeaderLine("##INFO=<ID=deaminations,Number=A,Type=Integer,Description=\"Total number of deaminations in the alternate allele\">");
+        variantFile.addHeaderLine("##INFO=<ID=aminations,Number=A,Type=Integer,Description=\"Total number of aminations in the alternate allele\">");
+        variantFile.addHeaderLine("##INFO=<ID=mismatches,Number=A,Type=Integer,Description=\"Total number of mismatches in the alternate allele\">");
+        variantFile.addHeaderLine("##INFO=<ID=insertions,Number=A,Type=Integer,Description=\"Total number of inserted bases in the alternate allele\">");
+        variantFile.addHeaderLine("##INFO=<ID=deletions,Number=A,Type=Integer,Description=\"Total number of deleted bases in the alternate allele\">");
+        cout << variantFile.header << endl;
     }
 
     Variant var(variantFile);
@@ -156,29 +196,51 @@ int main(int argc, char** argv) {
             }
             uniqueVariantAlleles += uniqueVariants.size();
             variantAlleles += var.alt.size();
+            map<string, AlleleStats> alleleStats;
 
             for (map<VariantAllele, vector<string> >::iterator v = uniqueVariants.begin(); v != uniqueVariants.end(); ++v) {
                 const VariantAllele& va = v->first;
-                //cout << va.ref << "/" << va.alt << endl;
+                vector<string>& alternates = v->second;
+                if (!addTags) { // don't add any tag information if we're not going to output it
+                    alternates.clear();
+                }
 
                 if (va.ref.size() == va.alt.size()) {
                     if (va.ref.size() == 1) {
                         ++snps;
                         ++mismatchbases;
+                        for (vector<string>::iterator a = alternates.begin(); a != alternates.end(); ++a) {
+                            ++alleleStats[*a].mismatches;
+                        }
                         if (isTransition(va.ref, va.alt)) {
                             ++transitions;
+                            for (vector<string>::iterator a = alternates.begin(); a != alternates.end(); ++a) {
+                                ++alleleStats[*a].transitions;
+                            }
                         } else {
                             ++transversions;
+                            for (vector<string>::iterator a = alternates.begin(); a != alternates.end(); ++a) {
+                                ++alleleStats[*a].transversions;
+                            }
                         }
                         if (isAmination(va.ref, va.alt)) {
                             ++aminations;
+                            for (vector<string>::iterator a = alternates.begin(); a != alternates.end(); ++a) {
+                                ++alleleStats[*a].aminations;
+                            }
                         }
                         if (isDeamination(va.ref, va.alt)) {
                             ++deaminations;
+                            for (vector<string>::iterator a = alternates.begin(); a != alternates.end(); ++a) {
+                                ++alleleStats[*a].deaminations;
+                            }
                         }
                     } else {
                         ++totalmnps;
                         ++mnps[va.alt.size()]; // not entirely correct
+                        for (vector<string>::iterator a = alternates.begin(); a != alternates.end(); ++a) {
+                            alleleStats[*a].mismatches += va.alt.size();
+                        }
                         string::const_iterator r = va.ref.begin();
                         for (string::const_iterator a = va.alt.begin(); a != va.alt.end(); ++a, ++r) {
                             string rstr = string(1, *r);
@@ -188,14 +250,26 @@ int main(int argc, char** argv) {
                             }
                             if (isTransition(rstr, astr)) {
                                 ++transitions;
+                                for (vector<string>::iterator a = alternates.begin(); a != alternates.end(); ++a) {
+                                    ++alleleStats[*a].transitions;
+                                }
                             } else {
                                 ++transversions;
+                                for (vector<string>::iterator a = alternates.begin(); a != alternates.end(); ++a) {
+                                    ++alleleStats[*a].transversions;
+                                }
                             }
                             if (isAmination(rstr, astr)) {
                                 ++aminations;
+                                for (vector<string>::iterator a = alternates.begin(); a != alternates.end(); ++a) {
+                                    ++alleleStats[*a].aminations;
+                                }
                             }
                             if (isDeamination(rstr, astr)) {
                                 ++deaminations;
+                                for (vector<string>::iterator a = alternates.begin(); a != alternates.end(); ++a) {
+                                    ++alleleStats[*a].deaminations;
+                                }
                             }
                             ++mismatchbases;
                             ++mnpbases;
@@ -206,12 +280,30 @@ int main(int argc, char** argv) {
                     deletedbases += diff;
                     ++totaldeletions;
                     ++deletions[diff];
+                    for (vector<string>::iterator a = alternates.begin(); a != alternates.end(); ++a) {
+                        alleleStats[*a].deletions += diff;
+                    }
                 } else {
                     int diff = va.alt.size() - va.ref.size();
                     insertedbases += diff;
                     ++totalinsertions;
                     ++insertions[diff];
+                    for (vector<string>::iterator a = alternates.begin(); a != alternates.end(); ++a) {
+                        alleleStats[*a].insertions += diff;
+                    }
                 }
+            }
+            if (addTags) {
+                for (vector<string>::iterator a = var.alt.begin(); a != var.alt.end(); ++a) {
+                    var.info["mismatches"].push_back(convert(alleleStats[*a].mismatches));
+                    var.info["insertions"].push_back(convert(alleleStats[*a].insertions));
+                    var.info["deletions"].push_back(convert(alleleStats[*a].deletions));
+                    var.info["transitions"].push_back(convert(alleleStats[*a].transitions));
+                    var.info["transversions"].push_back(convert(alleleStats[*a].transversions));
+                    var.info["deaminations"].push_back(convert(alleleStats[*a].deaminations));
+                    var.info["aminations"].push_back(convert(alleleStats[*a].aminations));
+                }
+                cout << var << endl;
             }
         }
 
@@ -240,52 +332,54 @@ int main(int argc, char** argv) {
 
     // now print the results
 
-    cout << "total variant sites:\t" << variantSites << endl
-         << "total variant alleles:\t" << variantAlleles << endl
-         << "unique variant alleles:\t" << uniqueVariantAlleles << endl
-         << endl
-         << "snps:\t" << snps << endl
-         << "mnps:\t" << totalmnps << endl
-         << "indels:\t" << totalinsertions + totaldeletions << endl
-         << "complex:\t" << totalcomplex << endl
-         << endl
-         << "mismatches:\t" << mismatchbases << endl
-         << endl
-         << "ts/tv ratio:\t" << (double) transitions / (double) transversions << endl
-         << "deamination ratio:\t" << (double) deaminations / aminations << endl
-         << endl
-         << "ins/del length frequency distribution" << endl
-         << "length\tins\tdel\tins/del" << endl;
-    for (int i = 1; i <= maxindel; ++i) {
-        int ins = insertions[i];
-        int del = deletions[i];
-        cout << i << "\t"
-             << (ins > 0 ? convert(ins) : "" ) << "\t"
-             << (del > 0 ? convert(del) : "") << "\t"
-             << (ins > 0 && del > 0 ? convert((double) ins / (double) del) : "")
-             << endl;
-    }
-    cout << endl
-         << "insertion alleles / deletion alleles:\t" << (double) totalinsertions / (double) totaldeletions << endl
-         << "inserted bases / deleted bases:\t" << (double) insertedbases / (double) deletedbases << endl
-         << endl
-         << "mnp length frequency distribution" << endl
-         << "length\tcount" << endl;
-    for (int i = 2; i <= maxmnp; ++i) {
-        int mnp = mnps[i];
-        cout << i << "\t"
-             << (mnp > 0 ? convert(mnp) : "")
-             << endl;
-    }
-    cout << "total bases in mnps:\t" << mnpbases << endl;
+    if (!addTags) {
+        cout << "total variant sites:\t" << variantSites << endl
+             << "total variant alleles:\t" << variantAlleles << endl
+             << "unique variant alleles:\t" << uniqueVariantAlleles << endl
+             << endl
+             << "snps:\t" << snps << endl
+             << "mnps:\t" << totalmnps << endl
+             << "indels:\t" << totalinsertions + totaldeletions << endl
+             << "complex:\t" << totalcomplex << endl
+             << endl
+             << "mismatches:\t" << mismatchbases << endl
+             << endl
+             << "ts/tv ratio:\t" << (double) transitions / (double) transversions << endl
+             << "deamination ratio:\t" << (double) deaminations / aminations << endl
+             << endl
+             << "ins/del length frequency distribution" << endl
+             << "length\tins\tdel\tins/del" << endl;
+        for (int i = 1; i <= maxindel; ++i) {
+            int ins = insertions[i];
+            int del = deletions[i];
+            cout << i << "\t"
+                 << (ins > 0 ? convert(ins) : "" ) << "\t"
+                 << (del > 0 ? convert(del) : "") << "\t"
+                 << (ins > 0 && del > 0 ? convert((double) ins / (double) del) : "")
+                 << endl;
+        }
+        cout << endl
+             << "insertion alleles / deletion alleles:\t" << (double) totalinsertions / (double) totaldeletions << endl
+             << "inserted bases / deleted bases:\t" << (double) insertedbases / (double) deletedbases << endl
+             << endl
+             << "mnp length frequency distribution" << endl
+             << "length\tcount" << endl;
+        for (int i = 2; i <= maxmnp; ++i) {
+            int mnp = mnps[i];
+            cout << i << "\t"
+                 << (mnp > 0 ? convert(mnp) : "")
+                 << endl;
+        }
+        cout << "total bases in mnps:\t" << mnpbases << endl;
 
-    /*
-    cout << "complex event frequency distribution" << endl
-         << "length\tcount" << endl;
-    for (map<int, int>::iterator i = complexsubs.begin(); i != complexsubs.end(); ++i) {
-        cout << i->first << "\t" << i->second << endl;
+        /*
+        cout << "complex event frequency distribution" << endl
+             << "length\tcount" << endl;
+        for (map<int, int>::iterator i = complexsubs.begin(); i != complexsubs.end(); ++i) {
+            cout << i->first << "\t" << i->second << endl;
+        }
+        */
     }
-    */
 
     return 0;
 
