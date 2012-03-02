33a377c0b1630236f95526169864a829bd42590d
arq5x <aaronquinlan@gmail.com>
2012-03-02 15:38:00 -0500

    changed test.py

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
 import pyvcflib
+import sys
 
-vcf_file = pyvcflib.VariantFile("sample.vcf")
+vcf_file = pyvcflib.VariantFile(sys.argv[1])
 for var in vcf_file:
     print var.chrom, var.pos, var.id, var.ref, \
           var.alt, var.qual, var.filter, var.info, \
-          var.format
-    # 
-    for sample in var.samples:
-        print sample + ":" + str(var.samples[sample]['GT'][0])
+          var.format, var.samples
