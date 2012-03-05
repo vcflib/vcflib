5ce42ae14f1ecd8854fb67f73608e365cc7ac687
arq5x <aaronquinlan@gmail.com>
2012-03-05 06:05:46 -0500

    updated test.py

--- test.py
+++ test.py
@@ -1,15 +1,18 @@
-import pyvcflib
+import pyvcflib as pvcf
 import sys
 
-vcf_file = pyvcflib.VariantFile(sys.argv[1])
+vcf_file = pvcf.VariantFile(sys.argv[1])
+
+# echo the file
+print vcf_file.header
 for var in vcf_file:
-    # print var.chrom, var.pos, var.id, var.ref, \
-    #       var.alt, var.qual, var.filter, var.info, \
-    #       var.format, var.aaf, var.pi, var.num_alleles, \
-    #       var.num_hom_ref, var.num_het, var.num_hom_alt, \
-    #       var.num_unknown, var.num_valid, var.num_alt_alleles
-          #var.samples \
+    print var
+
+# print individual attributes
+for var in pvcf.VariantFile(sys.argv[1]):
     print var.chrom, var.pos, var.id, var.ref, \
         var.alt, var.num_hom_ref, \
-        var.num_het, var.num_hom_alt, var.gts[0:10], var.gt_types[0:10]
-        #var.num_unknown, var.num_valid, var.num_alt_alleles
+        var.num_het, var.num_hom_alt, \
+        var.num_unknown, var.num_valid, var.num_alt_alleles, \
+        var.gts[0:10], var.gt_types[0:10], var.gt_phases[0:10], \
+        var.samples
