0435a934e9c06e1bf059bd57726f1953599a005d
Erik Garrison <erik.garrison@bc.edu>
2012-07-30 16:31:08 -0400

    update vcfcleancomplex

--- vcfcleancomplex.cpp
+++ vcfcleancomplex.cpp
@@ -40,21 +40,28 @@ int main(int argc, char** argv) {
     // print the records, filtering is done via the setting of varA's output sample names
     while (variantFile.getNextVariant(var)) {
         // if we just have one parsed alternate (non-complex case)
-        map<string, vector<VariantAllele> > parsedAlts = var.parsedAlternates();
+        map<string, vector<VariantAllele> > parsedAlts = var.parsedAlternates(true, true); // use mnps, and previous for indels
         // but the alt string is long
         if (var.alt.size() == 1 && parsedAlts.size() == 2) {
             string& alternate = var.alt.front();
-            if (parsedAlts[alternate].size() == 1) {
+	    vector<VariantAllele>& vs = parsedAlts[alternate];
+	    vector<VariantAllele> valleles;
+	    for (vector<VariantAllele>::iterator a = vs.begin(); a != vs.end(); ++a) {
+		if (a->ref != a->alt) {
+		    valleles.push_back(*a); //cout << a->ref << " " << a->alt << endl;
+		}
+	    }
+            if (valleles.size() == 1) {
                 // do we have extra sequence hanging around?
-                VariantAllele& varallele = parsedAlts[alternate].front();
+                VariantAllele& varallele = valleles.front();
+		//cout << varallele.ref << " " << varallele.alt << endl;
                 // for deletions and insertions, we have to keep a leading base
                 // but the variant allele doesn't have these
                 if (varallele.ref.size() == varallele.alt.size()) {
-                    if (varallele.position != var.position) {
-                        var.ref = varallele.ref;
-                        var.alt.front() = varallele.alt;
-                        var.position = varallele.position;
-                    }
+                    //if (varallele.position != var.position) {
+		    var.ref = varallele.ref;
+		    var.alt.front() = varallele.alt;
+		    var.position = varallele.position;
                 } else if (varallele.ref.size() < varallele.alt.size()) {
                     if (varallele.position != var.position + 1) {
                         // TODO unhandled
