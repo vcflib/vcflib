7d19dfed6bd3461a1478a02c34bf0e38e2ec1b3a
Erik Garrison <erik.garrison@bc.edu>
2013-04-23 19:57:14 -0400

    update to vcflength, use +/- instead of abs(...)

--- vcflength.cpp
+++ vcflength.cpp
@@ -25,11 +25,11 @@ int main(int argc, char** argv) {
 
     Variant var(variantFile);
     while (variantFile.getNextVariant(var)) {
-	vector<string>& lengths = var.info["length"];
-	lengths.clear();
-	for (vector<string>::iterator a = var.alt.begin(); a != var.alt.end(); ++a) {
-	    lengths.push_back(convert(abs((int) a->size() - (int) var.ref.size())));
-	}
+        vector<string>& lengths = var.info["length"];
+        lengths.clear();
+        for (vector<string>::iterator a = var.alt.begin(); a != var.alt.end(); ++a) {
+            lengths.push_back(convert((int) a->size() - (int) var.ref.size()));
+        }
         cout << var << endl;
     }
 
