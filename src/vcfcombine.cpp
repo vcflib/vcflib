569efcb8d90d4dba1f46e58a6e4ad38d12add48a
Erik Garrison <erik.garrison@bc.edu>
2013-10-17 21:22:33 -0400

    missing file from previous commit

---
+++ src/vcfcombine.cpp
@@ -0,0 +1,127 @@
+#include "Variant.h"
+#include <getopt.h>
+
+using namespace std;
+using namespace vcf;
+
+void printSummary(char** argv) {
+    cerr << "usage: " << argv[0] << " [vcf file] [vcf file] ..." << endl
+         << endl
+         << "Combines VCF files positionally, combining samples when sites and alleles are identical." << endl
+         << "Any number of VCF files may be combined.  The INFO field and other columns are taken from" << endl
+         << "one of the files which are combined when records in multiple files match.  Alleles must" << endl
+         << "have identical ordering to be combined into one record.  If they do not, multiple records" << endl
+         << "will be emitted." << endl;
+    exit(1);
+}
+
+
+int main(int argc, char** argv) {
+
+    int c;
+    while (true) {
+        static struct option long_options[] =
+        {
+            /* These options set a flag. */
+            //{"verbose", no_argument,       &verbose_flag, 1},
+            {"help", no_argument, 0, 'h'},
+            {0, 0, 0, 0}
+        };
+        /* getopt_long stores the option index here. */
+        int option_index = 0;
+
+        c = getopt_long (argc, argv, "h",
+                         long_options, &option_index);
+
+        if (c == -1)
+            break;
+
+        switch (c) {
+
+        case 'h':
+            printSummary(argv);
+            break;
+            
+        case '?':
+            printSummary(argv);
+            exit(1);
+            break;
+                
+        default:
+            abort ();
+        }
+    }
+
+    vector<string> sampleNames;
+    string randomHeader;
+    VariantCallFile* vcf;
+
+    map<string, // chrom
+        map<long int, // pos
+            map<vector<string>, // alts
+                map<VariantCallFile*, Variant*> > > > variantFiles;
+    for (int i = optind; i != argc; ++i) {
+        string inputFilename = argv[i];
+        vcf = new VariantCallFile;
+        vcf->open(inputFilename);
+        Variant* var = new Variant(*vcf);
+        vcf->getNextVariant(*var);
+        variantFiles[var->sequenceName][var->position][var->alt][vcf] = var;
+        sampleNames.insert(sampleNames.end(), vcf->sampleNames.begin(), vcf->sampleNames.end());
+    }
+
+    sort(sampleNames.begin(), sampleNames.end());
+    sampleNames.erase(unique(sampleNames.begin(), sampleNames.end()), sampleNames.end());
+
+    VariantCallFile outputCallFile;
+    string header = vcf->headerWithSampleNames(sampleNames);
+    outputCallFile.openForOutput(header);
+
+    cout << outputCallFile.header << endl;
+
+    while (!variantFiles.empty()) {
+        // get lowest variant(s)
+        // if they have identical alts and position, combine
+        // otherwise just output, but with the same sample names
+        map<long int, map<vector<string>, map<VariantCallFile*, Variant*> > >& chrom = variantFiles.begin()->second;
+        if (chrom.empty()) {
+            variantFiles.erase(variantFiles.begin());
+            continue;
+        }
+        map<vector<string>, map<VariantCallFile*, Variant*> >& pos = chrom.begin()->second;
+        map<vector<string>, map<VariantCallFile*, Variant*> >::iterator s = pos.begin();
+        for ( ; s != pos.end(); ++s) {
+            vector<string> alt;
+            Variant variant(outputCallFile);
+            variant.alt = alt;
+            map<VariantCallFile*, Variant*>& vars = s->second;
+            map<VariantCallFile*, Variant*>::iterator v = vars.begin();
+            for ( ; v != vars.end(); ++v) {
+                VariantCallFile* vcf = v->first;
+                Variant* var = v->second;
+                if (variant.info.empty()) {
+                    variant.sequenceName = var->sequenceName;
+                    variant.position = var->position;
+                    variant.id = var->id;
+                    variant.ref = var->ref;
+                    variant.alt = var->alt;
+                    variant.quality = var->quality;
+                    variant.info = var->info;
+                    variant.format = var->format;
+                }
+                // add samples to output variant
+                for (Samples::iterator sample = var->samples.begin(); sample != var->samples.end(); ++sample) {
+                    variant.samples[sample->first] = sample->second;
+                }
+                if (vcf->getNextVariant(*var))
+                    variantFiles[var->sequenceName][var->position][var->alt][vcf] = var;
+            }
+            cout << variant << endl;
+        }
+        chrom.erase(chrom.begin());
+    }
+
+    return 0;
+
+}
+
