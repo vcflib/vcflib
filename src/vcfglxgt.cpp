f535c98de0f0714edc3d6bf8bcf28a450147c05a
Erik Garrison <erik.garrison@bc.edu>
2014-01-05 20:29:16 -0500

    handle cases where only GLs are reported in vcfglxgt

--- src/vcfglxgt.cpp
+++ src/vcfglxgt.cpp
@@ -93,64 +93,76 @@ int main(int argc, char** argv) {
 
     Variant var(variantFile);
     while (variantFile.getNextVariant(var)) {
-	for (map<string, map<string, vector<string> > >::iterator s = var.samples.begin();
-	     s != var.samples.end(); ++s) {
-	    map<string, vector<string> >& sample = s->second;
-	    map<string, vector<string> >::iterator g = sample.find("GT");
-	    map<string, vector<string> >::iterator l = sample.find("GL");
-	    if (g != sample.end() && l != sample.end()) {
+        if (find(var.format.begin(), var.format.end(), "GL") == var.format.end()) {
+            cout << var << endl;
+            continue;
+        }
+        if (find(var.format.begin(), var.format.end(), "GT") == var.format.end()) {
+            var.format.push_back("GT");
+            reverse(var.format.begin(), var.format.end());
+        }
+        for (map<string, map<string, vector<string> > >::iterator s = var.samples.begin();
+             s != var.samples.end(); ++s) {
+            map<string, vector<string> >& sample = s->second;
+            map<string, vector<string> >::iterator g = sample.find("GT");
+            map<string, vector<string> >::iterator l = sample.find("GL");
+            if (l != sample.end()) {
+                if (g == sample.end()) {
+                    sample["GT"].push_back("./.");
+                    g = sample.find("GT");
+                }
 
-		string& gt = g->second.front();
-		// if we are fixing null but the genotype is fully specified, continue
-		if (fixNull && gt.find(".") == string::npos) continue;
-		string splitter = "/";
-		if (gt.find("|") != string::npos) splitter = "|";
-		int samplePloidy = split(gt, splitter).size();
-		int numAlleles = var.alt.size() + 1; // including reference
+                string& gt = g->second.front();
+                // if we are fixing null but the genotype is fully specified, continue
+                if (fixNull && gt.find(".") == string::npos) continue;
+                string splitter = "/";
+                if (gt.find("|") != string::npos) splitter = "|";
+                int samplePloidy = split(gt, splitter).size();
+                int numAlleles = var.alt.size() + 1; // including reference
 
-		// get the gt GL ordering
-		pair<int, int> pa = make_pair(samplePloidy, numAlleles);
-		map<pair<int, int>, list<list<int> > >::iterator order = glOrderCache.find(pa);
-		if (order == glOrderCache.end()) {
-		    glOrderCache[pa] = glorder(samplePloidy, numAlleles);
-		}
-		list<list<int> >& glOrdering = glOrderCache[pa];
+                // get the gt GL ordering
+                pair<int, int> pa = make_pair(samplePloidy, numAlleles);
+                map<pair<int, int>, list<list<int> > >::iterator order = glOrderCache.find(pa);
+                if (order == glOrderCache.end()) {
+                    glOrderCache[pa] = glorder(samplePloidy, numAlleles);
+                }
+                list<list<int> >& glOrdering = glOrderCache[pa];
 
-		// find the gl max
-		vector<string>& gls = l->second;
-		vector<string>::iterator p = gls.begin();
-		double maxGl;
-		convert(*p, maxGl); ++p;
-		int i = 1, maxindex = 0;
-		for (; p != gls.end(); ++p, ++i) {
-		    double cgl;
-		    convert(*p, cgl);
-		    if (cgl > maxGl) {
-			maxGl = cgl;
-			maxindex = i; // prefers == gls in order of listing
-		    }
-		}
+                // find the gl max
+                vector<string>& gls = l->second;
+                vector<string>::iterator p = gls.begin();
+                double maxGl;
+                convert(*p, maxGl); ++p;
+                int i = 1, maxindex = 0;
+                for (; p != gls.end(); ++p, ++i) {
+                    double cgl;
+                    convert(*p, cgl);
+                    if (cgl > maxGl) {
+                        maxGl = cgl;
+                        maxindex = i; // prefers == gls in order of listing
+                    }
+                }
 		
-		// determine which genotype it represents
-		// modify, if the GT is part-null
-		vector<string>& gtv = g->second;
-	        list<list<int> >::iterator b = glOrdering.begin();
-		advance(b, maxindex);
-		/*
-		cout << "changing sample " << s->first << " gt from " << gt << " to " << join(*b, "/")
-		     << " gls are ";
-		int q = 0;
-		for (list<list<int> >::iterator i = glOrdering.begin(); i != glOrdering.end(); ++i, ++q) {
-		    cout << join(*i, "/") << ":" << sample["GL"].at(q) << ", ";
-		}
-		cout << endl;
-		*/
+                // determine which genotype it represents
+                // modify, if the GT is part-null
+                vector<string>& gtv = g->second;
+                list<list<int> >::iterator b = glOrdering.begin();
+                advance(b, maxindex);
+                /*
+                  cout << "changing sample " << s->first << " gt from " << gt << " to " << join(*b, "/")
+                  << " gls are ";
+                  int q = 0;
+                  for (list<list<int> >::iterator i = glOrdering.begin(); i != glOrdering.end(); ++i, ++q) {
+                  cout << join(*i, "/") << ":" << sample["GL"].at(q) << ", ";
+                  }
+                  cout << endl;
+                */
 
-		gtv.clear();
-		gtv.push_back(join(*b, "/"));
-	    }
-	}
-	cout << var << endl;
+                gtv.clear();
+                gtv.push_back(join(*b, "/"));
+            }
+        }
+        cout << var << endl;
     }
 
     return 0;
