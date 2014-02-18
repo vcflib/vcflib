d7dc6334892b0565764743f9c25244fd4d228c45
Erik Garrison <erik.garrison@bc.edu>
2014-02-18 07:47:43 -0500

    add ref and alt lengths to vcflength
    These are helpful in some contexts, when it would be nice to know
    the length of a haplotype call for instance, rather than the
    absolute delta of the ref/alt pair.

--- src/vcflength.cpp
+++ src/vcflength.cpp
@@ -21,6 +21,8 @@ int main(int argc, char** argv) {
     }
 
     variantFile.addHeaderLine("##INFO=<ID=length,Number=A,Type=Integer,Description=\"length(ALT) - length(REF) for each ALT\">");
+    variantFile.addHeaderLine("##INFO=<ID=length.ref,Number=1,Type=Integer,Description=\"length(REF)\">");
+    variantFile.addHeaderLine("##INFO=<ID=length.alt,Number=A,Type=Integer,Description=\"length(ALT) for each ALT\">");
     cout << variantFile.header << endl;
 
     Variant var(variantFile);
@@ -29,6 +31,14 @@ int main(int argc, char** argv) {
         lengths.clear();
         for (vector<string>::iterator a = var.alt.begin(); a != var.alt.end(); ++a) {
             lengths.push_back(convert((int) a->size() - (int) var.ref.size()));
+        }
+        vector<string>& lengthsRef = var.info["length.ref"];
+        lengthsRef.clear();
+        lengthsRef.push_back(convert(var.ref.size()));
+        vector<string>& lengthsAlt = var.info["length.alt"];
+        lengthsAlt.clear();
+        for (vector<string>::iterator a = var.alt.begin(); a != var.alt.end(); ++a) {
+            lengthsAlt.push_back(convert((int) a->size()));
         }
         cout << var << endl;
     }
