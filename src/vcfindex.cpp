b47299880e5fbd9d9a98d7ff24d597735f7494c9
Erik Garrison <erik.garrison@bc.edu>
2014-02-24 09:53:23 -0500

    added vcfindex
    This utility adds unique ids for each allele as fields in info.  This works
    better than the actual ID field because you the count of the id field can be
    'A' and thus mapped to the alleles at the locus.

---
+++ src/vcfindex.cpp
@@ -0,0 +1,42 @@
+#include "Variant.h"
+#include "convert.h"
+#include <vector>
+
+using namespace std;
+using namespace vcf;
+
+int main(int argc, char** argv) {
+
+    VariantCallFile variantFile;
+
+    if (argc > 1) {
+        string filename = argv[1];
+        variantFile.open(filename);
+    } else {
+        variantFile.open(std::cin);
+    }
+
+    if (!variantFile.is_open()) {
+        return 1;
+    }
+
+    string idname = "id";
+    long int uid = 0;
+
+    variantFile.addHeaderLine("##INFO=<ID="+idname+",Number=A,Type=Integer,Description=\"Unique numerical identifier of allele in file.\">");
+    cout << variantFile.header << endl;
+
+    Variant var(variantFile);
+    while (variantFile.getNextVariant(var)) {
+        vector<string>& idxs = var.info[idname];
+        idxs.clear();
+        for (vector<string>::iterator a = var.alt.begin(); a != var.alt.end(); ++a) {
+            idxs.push_back(convert(uid++));
+        }
+        cout << var << endl;
+    }
+
+    return 0;
+
+}
+
