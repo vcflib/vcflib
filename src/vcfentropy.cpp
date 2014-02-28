95e3579069cbb211e9ce4e91f3fd79c3a18a9372
Erik Garrison <erik.garrison@bc.edu>
2014-02-27 19:23:46 -0500

    improvements to vcfentropy

--- src/vcfentropy.cpp
+++ src/vcfentropy.cpp
@@ -10,12 +10,12 @@ void printSummary(char** argv) {
     cerr << "usage: " << argv[0] << " [options] <vcf file>" << endl
          << endl
          << "options:" << endl 
-         << "    -f, --fasta-reference  FASTA reference file to use to obtain primer sequences" << endl
+         << "    -f, --fasta-reference  FASTA reference file to use to obtain flanking sequences" << endl
          << "    -w, --window-size      Size of the window over which to calculate entropy" << endl
          << endl
-	 << "Anotates the output VCF file with, for each record, EntropyLeft, EntropyRight," << endl
-	 << "EntropyCenter, which are the entropies of the sequence of the given window size to the" << endl
-	 << "left, right, and center  of the record." << endl
+         << "Anotates the output VCF file with, for each record, EntropyLeft, EntropyRight," << endl
+         << "EntropyCenter, which are the entropies of the sequence of the given window size to the" << endl
+         << "left, right, and center  of the record.  Also adds EntropyRef and EntropyAlt for each alt." << endl
          << endl;
     exit(0);
 }
@@ -115,23 +115,40 @@ int main(int argc, char** argv) {
     variantFile.addHeaderLine("##INFO=<ID=EntropyLeft,Number=1,Type=Float,Description=\"Entropy of left-flanking sequence of "+ convert(windowSize) +"bp\">");
     variantFile.addHeaderLine("##INFO=<ID=EntropyCenter,Number=1,Type=Float,Description=\"Entropy of centered sequence of "+ convert(windowSize) +"bp\">");
     variantFile.addHeaderLine("##INFO=<ID=EntropyRight,Number=1,Type=Float,Description=\"Entropy of right-flanking sequence of "+ convert(windowSize) +"bp\">");
+    variantFile.addHeaderLine("##INFO=<ID=EntropyRef,Number=1,Type=Float,Description=\"Entropy of REF allele\">");
+    variantFile.addHeaderLine("##INFO=<ID=EntropyAlt,Number=A,Type=Float,Description=\"Entropy of each ALT allele\">");
 
     cout << variantFile.header << endl;
 
     Variant var(variantFile);
     while (variantFile.getNextVariant(var)) {
+
         // get the ref start and end positions
         int refstart = var.position - 1; // convert to 0-based
         int refend = var.position + var.ref.size() - 1;
         string leftseq = ref.getSubSequence(var.sequenceName, refstart - windowSize, windowSize);
         string rightseq = ref.getSubSequence(var.sequenceName, refend, windowSize);
-	string centerseq = ref.getSubSequence(var.sequenceName, refstart - windowSize/2, windowSize);
-	double entropyLeft = shannon_H((char*) &leftseq[0], windowSize);
-	double entropyRight = shannon_H((char*) &rightseq[0], windowSize);
-	double entropyCenter = shannon_H((char*) &centerseq[0], windowSize);
-	var.info["EntropyLeft"].push_back(convert(entropyLeft));
-	var.info["EntropyRight"].push_back(convert(entropyRight));
-	var.info["EntropyCenter"].push_back(convert(entropyCenter));
+        string centerseq = ref.getSubSequence(var.sequenceName, refstart - windowSize/2, windowSize);
+        double entropyLeft = shannon_H((char*) &leftseq[0], windowSize);
+        double entropyRight = shannon_H((char*) &rightseq[0], windowSize);
+        double entropyCenter = shannon_H((char*) &centerseq[0], windowSize);
+        double entropyRef = shannon_H((char*) var.ref.c_str(), var.ref.size());
+
+        var.info["EntropyLeft"].clear();
+        var.info["EntropyRight"].clear();
+        var.info["EntropyCenter"].clear();
+        var.info["EntropyRef"].clear();
+        var.info["EntropyAlt"].clear();
+
+        var.info["EntropyLeft"].push_back(convert(entropyLeft));
+        var.info["EntropyRight"].push_back(convert(entropyRight));
+        var.info["EntropyCenter"].push_back(convert(entropyCenter));
+        var.info["EntropyRef"].push_back(convert(entropyRef));
+
+        for (vector<string>::iterator a = var.alt.begin(); a != var.alt.end(); ++a) {
+            double entropyAlt = shannon_H((char*) a->c_str(), a->size());
+            var.info["EntropyAlt"].push_back(convert(entropyAlt));
+        }
 
         cout << var << endl;
     }
