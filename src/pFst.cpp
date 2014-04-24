c6188bfc5a16f7783fc35ffc45fff0ae5d9338e0
zkronenb <zkronenb@browning.genetics.utah.edu>
2014-04-24 11:17:05 -0600

    missing other phase info shit

--- src/pFst.cpp
+++ src/pFst.cpp
@@ -120,6 +120,14 @@ void loadPop( vector< map< string, vector<string> > >& group, pop & population){
 	population.geno_index.push_back(1);
 	break;
       }
+      if(genotype == "1|0"){
+        population.ngeno += 1;
+        population.nhet  += 1;
+        population.nref  += 1;
+        population.nalt  += 1;
+        population.geno_index.push_back(1);
+        break;
+      }
       if(genotype == "1|1"){
 	population.ngeno += 1;
 	population.nhoma += 1;
@@ -127,7 +135,7 @@ void loadPop( vector< map< string, vector<string> > >& group, pop & population){
 	population.geno_index.push_back(2);
 	break;
       }
-      cerr << "FATAL: unknown genotype" << endl;
+      cerr << "FATAL: unknown genotype: " << genotype << endl;
       exit(1);
     }
   }
