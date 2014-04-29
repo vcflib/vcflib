2011d7863091cde242449190c68a012dd586e7c1
jewmanchue <zev.kronenberg@gmail.com>
2014-04-28 18:00:24 -0600

    Update plotSmoothed.R
    added wcFst support in if statements

--- bin/plotSmoothed.R
+++ bin/plotSmoothed.R
@@ -1,13 +1,21 @@
-#usage:  nohup R --vanilla < plotPfst --args smoothedpFst.txt
+#usage:  nohup R --vanilla < plotPfst --args smoothedpFst.txt wcFst|pFst
 
 cmd_args <- commandArgs(trailingOnly = TRUE)
 
 plotPfst<-function(x){
 	require("ggplot2")
-	dat<-read.table( x, header=FALSE )
+	dat<-read.table( x[1], header=FALSE )
 	dat$V2<-1:length(dat$V2)
-	theplot<-ggplot(dat, aes(x=V2, y=-log10(V5), colour=V4))+geom_point()+theme_grey(15)+labs(x="SNP index", y="-log10(smoothed pFst)")+scale_colour_continuous(low="grey", high="red", name="variants in window")
 	pngName<-paste(c(x, format(Sys.time(), "%a%b%d_%H_%M_%S.png")), collapse="_")
+
+	theplot<-NULL
+
+	if(x[2] == "pFst"){
+		theplot<-ggplot(dat, aes(x=V2, y=-log10(V5), colour=V4))+geom_point()+theme_grey(15)+labs(x="SNP index", y="-log10(smoothed pFst)")+scale_colour_continuous(low="grey", high="red", name="variants in window")
+	}
+	if(x[2] == "wcFst"){
+		theplot<-ggplot(dat, aes(x=V2, y=-log10(V5), colour=V4))+geom_point()+theme_grey(15)+labs(x="SNP index", y="-log10(smoothed pFst)")+scale_colour_continuous(low="grey", high="red", name="variants in window")
+	}
 	ggsave(filename=pngName, width=20, height=4, units="in", theplot)
 }
 
