8f3865ad92991c6c557ae7024255d1f6e164549b
BrettKennedy <brett.kennedy@utah.edu>
2014-05-02 12:42:33 -0600

    Added the 1KG indexing script

---
+++ 1KG_Index/1kG_ethnic_index.py
@@ -0,0 +1,51 @@
+import argparse, csv
+
+parser=argparse.ArgumentParser(description="Determines the index of individuals\
+ of a given ethnicity within a 1000 Genomes VCF")
+parser.add_argument("VCF", type=str, help="VCF of 1000 Genomes individuals")
+parser.add_argument("Population", type=str, help="1KG identifier for population\
+to be found in the index, enter \"ALL|\" to print index for all populations in \
+the VCF")
+arg=parser.parse_args()
+
+try:
+	 open("1KG_IDs.txt")
+except IOError:
+	print "Missing file \"1KG_IDs.txt\" containing the IDs for 1000 Genomes"
+
+popdict={}
+with open("1KG_IDs.txt") as t:
+	for line in csv.reader(t,delimiter='\t'):
+		popdict[line[1]]=line[0]
+
+with open(arg.VCF) as t:
+	for line in csv.reader(t,delimiter='\t'):
+		if line[0]=='#CHROM':
+			for j in range(len(line)):
+				if 'NA' in line[j] or 'HG' in line[j]:
+					vcfind=line[j:]
+					break
+
+indict={}
+for i in range(len(vcfind)):
+	try:
+		eth=popdict[vcfind[i]]
+	except:
+		print"Non 1000 Genomes individuals found in VCF"
+	if eth in indict.keys():
+		indict[eth].append(i)
+	else:
+		indict[eth]=[i]
+
+if arg.Population=="All":
+	for j in indict.keys():
+		l=[str(i) for i in indict[arg.Population]]
+		print j,"\t",",".join(l)
+
+else:
+	try:
+		indict[arg.Population]
+	except IOError:
+		print "Population not in VCF or invalid population ID"
+	l=[str(i) for i in indict[arg.Population]]
+	print ",".join(l)
