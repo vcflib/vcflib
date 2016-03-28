a3173c2955df080f664985bcde60d55d4b52ffa3
Zev N. Kronenberg <zevk@e186.grid.gs.washington.edu>
2016-03-28 10:55:50 -0700

    basic tests now running

--- test/tests/variantTests.h
+++ test/tests/variantTests.h
@@ -1,11 +1,46 @@
 #include "gtest/gtest.h"
 #include "Variant.h"
+#include <iostream>
 
-TEST(VariantCallFile, A){
+TEST(VariantCallFile, open){
 
   vcflib::VariantCallFile variantFile;
 
-  std::string file = "../../samples/sample.vcf";
+  std::string filename = "../samples/sample.vcf";
+
+  variantFile.open(filename);
+  
+  ASSERT_TRUE(variantFile.is_open());
 
 
 };
+
+
+TEST(VariantCallFile, recordCountUncompressed){
+  vcflib::VariantCallFile variantFile;
+  
+  std::string filename = "../samples/sample.vcf";
+  variantFile.open(filename);
+  vcflib::Variant var(variantFile);
+  
+  long int count = 0;
+
+  while (variantFile.getNextVariant(var)) {
+    count+= 1;
+  }
+  ASSERT_EQ(count, 9);
+}
+
+TEST(VariantCallFile, recordCountCompressed){
+  vcflib::VariantCallFile variantFile;
+
+  std::string filename = "../samples/sample.compressed.vcf.gz";
+  variantFile.open(filename);
+  vcflib::Variant var(variantFile);
+
+  long int count = 0;
+  while (variantFile.getNextVariant(var)) {
+    count+= 1;
+  }
+  ASSERT_EQ(count, 9);
+}
