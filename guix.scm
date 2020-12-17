da6505f4758b865a09fcf53fa619ff1389617966
Pjotr Prins <pjotr.public01@thebird.nl>
2020-12-17 07:02:18 +0000

    Adding guix.scm

---
+++ guix.scm
@@ -0,0 +1,74 @@
+;; To use this file to build HEAD of freebayes:
+;;
+;;   guix build -f guix.scm
+;;
+;; To get a development container (emacs shell will work)
+;;
+;;   guix environment -C -l guix.scm
+
+(use-modules
+  ((guix licenses) #:prefix license:)
+  (guix gexp)
+  (guix packages)
+  (guix git-download)
+  (guix build-system cmake)
+  (gnu packages algebra)
+  (gnu packages base)
+  (gnu packages compression)
+  (gnu packages bioinformatics)
+  (gnu packages build-tools)
+  (gnu packages curl)
+  (gnu packages llvm)
+  (gnu packages python)
+  ;; (gnu packages ninja)
+  (gnu packages parallel)
+  (gnu packages perl)
+  (gnu packages perl6)
+  (gnu packages pkg-config)
+  (srfi srfi-1)
+  (ice-9 popen)
+  (ice-9 rdelim))
+
+;;           (substitute* (find-files "." "\\.(h|c)(pp)?$")
+;;             (("\"SmithWatermanGotoh.h\"") "<smithwaterman/SmithWatermanGotoh.h>")
+;;             (("\"convert.h\"") "<smithwaterman/convert.h>")
+;;             (("\"disorder.h\"") "<smithwaterman/disorder.h>")
+;;            (("\"tabix.hpp\"") "<tabix.hpp>")
+;;             (("\"Fasta.h\"") "<fastahack/Fasta.h>"))
+;;           (for-each delete-file-recursively
+;;                     '("fastahack" "filevercmp" "fsom" "googletest" "intervaltree"
+;;                       "libVCFH" "multichoose" "smithwaterman" "tabixpp"))
+
+(define %source-dir (dirname (current-filename)))
+
+(define %git-commit
+    (read-string (open-pipe "git show HEAD | head -1 | cut -d ' ' -f 2" OPEN_READ)))
+
+(define-public vcflib-git
+  (package
+    (name "vcflib-git")
+    (version (git-version "1.0.2" "HEAD" %git-commit))
+    (source (local-file %source-dir #:recursive? #t))
+    (build-system cmake-build-system)
+    (inputs
+     `(("curl" ,curl)
+       ("fastahack" ,fastahack)
+       ("htslib" ,htslib)
+       ("perl" ,perl)
+       ("python" ,python)
+       ("smithwaterman" ,smithwaterman)
+       ("tabixpp" ,tabixpp)
+       ("xz" ,xz)
+       ("zlib" ,zlib)))
+    (native-inputs
+     `(("pkg-config" ,pkg-config)))
+    (home-page "https://github.com/vcflib/vcflib/")
+    (synopsis "Library for parsing and manipulating VCF files")
+    (description "Vcflib provides methods to manipulate and interpret
+sequence variation as it can be described by VCF.  It is both an API for parsing
+and operating on records of genomic variation as it can be described by the VCF
+format, and a collection of command-line utilities for executing complex
+manipulations on VCF files.")
+    (license license:expat)))
+
+vcflib-git
