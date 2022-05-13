4fa999c2e035cea5a21877b4c8d0850206e11ca1
Pjotr Prins <pjotr.public01@thebird.nl>
2022-05-13 08:10:41 +0200

    Reproduce vcfwave issue for 10134518

--- src/pythonffi.cpp
+++ src/pythonffi.cpp
@@ -19,12 +19,26 @@ PYBIND11_MODULE(pyvcflib, m)
   m.doc() = "This is a Python binding of C++ vcflib Library with WFA";
 
   // Wavefront
+
+  py::class_<affine2p_penalties_t>(m, "affine2p_penalties_t", "WFA affine2 settings")
+      .def_readwrite("match", &affine2p_penalties_t::match)
+      .def_readwrite("mismatch", &affine2p_penalties_t::mismatch)
+      .def_readwrite("gap_opening1", &affine2p_penalties_t::gap_opening1)
+      .def_readwrite("gap_extension1", &affine2p_penalties_t::gap_extension1)
+      .def_readwrite("gap_opening2", &affine2p_penalties_t::gap_opening2)
+      .def_readwrite("gap_extension2", &affine2p_penalties_t::gap_extension2)
+      ;
   py::class_<wavefront_aligner_attr_t>(m, "wavefront_aligner_attr_t", "WFA settings")
       .def_readwrite("distance_metric", &wavefront_aligner_attr_t::distance_metric)
+      .def_readwrite("affine2p_penalties", &wavefront_aligner_attr_t::affine2p_penalties)
+      .def_readwrite("alignment_scope", &wavefront_aligner_attr_t::alignment_scope)
       ;
   m.attr("wavefront_aligner_attr_default") = wavefront_aligner_attr_default;
   py::enum_<distance_metric_t>(m, "distance_meric_t")
-    .value("gap_affine_2p", gap_affine_2p)
+      .value("gap_affine_2p", gap_affine_2p)
+      ;
+  py::enum_<alignment_scope_t>(m, "alignment_scope_t")
+      .value("compute_alignment", compute_alignment)
       ;
 
 
--- test/tests/realign.py
+++ test/tests/realign.py
@@ -18,9 +18,9 @@ class RealignTest(unittest.TestCase):
         self.assertEqual(rec.name,"grch38#chr4")
         self.assertEqual(rec.ref,'ACCCCCACCCCCACC')
         self.assertEqual(rec.alt,['ACC', 'AC', 'ACCCCCACCCCCAC', 'ACCCCCACC', 'ACA'])
-        newvcf = rec.legacy_parsedAlternates(False,False,False,10.0,-9.0,15.0,6.66,0.0,"","",False,True)
+        sw = rec.legacy_parsedAlternates(False,False,False,10.0,-9.0,15.0,6.66,0.0,"","",False,True)
 
-    def test_wftailbug(self):
+    def test_sw_wf_compare(self):
         vcf = VariantCallFile()
         vcf.openFile("../samples/10134514.vcf")
         rec = Variant(vcf)
@@ -28,17 +28,20 @@ class RealignTest(unittest.TestCase):
         self.assertEqual(rec.name,"grch38#chr4_10083863-10181258.vcf:grch38#chr4")
         self.assertEqual(rec.ref,'GGAGAATCCCAATTGATGG')
         self.assertEqual(rec.alt,['GTAGCATCCCAAGTGATGT', 'GTAGAATCCCAATTGATGT', 'GGAGCATCCCAATTGATGG', 'GG'])
-        sw = rec.legacy_parsedAlternates(False,False,False,10.0,-9.0,15.0,6.66,0.0,"","",False,True)
+        sw = rec.legacy_parsedAlternates(False,False,False,10.0,-9.0,15.0,6.66,0.0,"","",False,False)
         for key, value in sw.items():
             print(f'SW allele key: {key}: ')
             for a in value:
                 print(f'               {a.position}/{a.ref}/{a.alt} ')
         # note wf ignores paramaters
-        wf = rec.legacy_parsedAlternates(False,False,False,10.0,-9.0,15.0,6.66,0.0,"","",True,True)
+        wf = rec.legacy_parsedAlternates(False,False,False,10.0,-9.0,15.0,6.66,0.0,"","",True,False)
         for key, value in wf.items():
             print(f'WF allele key: {key}: ')
             for a in value:
                 print(f'               {a.position}/{a.ref}/{a.alt} ')
+        self.assertEqual(len(wf),5)
+        self.assertEqual(len(wf),len(sw))
+        self.assertEqual(sw['GGAGAATCCCAATTGATGG'][0].alt,wf['GGAGAATCCCAATTGATGG'][0].alt)
 
     def test_wfbug2(self):
         vcf = VariantCallFile()
@@ -51,6 +54,13 @@ class RealignTest(unittest.TestCase):
         wfa_params = wavefront_aligner_attr_default
         # string paramString = "0,19,39,3,81,1";
         wfa_params.distance_metric = distance_meric_t.gap_affine_2p
+        wfa_params.affine2p_penalties.match = 0
+        wfa_params.affine2p_penalties.mismatch = 19
+        wfa_params.affine2p_penalties.gap_opening1 = 39
+        wfa_params.affine2p_penalties.gap_extension1 = 3
+        wfa_params.affine2p_penalties.gap_opening2 = 81
+        wfa_params.affine2p_penalties.gap_extension2 = 1
+        wfa_params.alignment_scope = alignment_scope_t.compute_alignment;
         wf = rec.parsedAlternates(False,True,False,"","",wfa_params,True,64,True)
         for key, value in wf.items():
             print(f'WF2 allele key: {key}: ')
